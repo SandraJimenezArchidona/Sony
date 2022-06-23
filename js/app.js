609787c0ee43b28f4b87c702876ffc0d788e875c
Sansandra <106440634+Sandrusky@users.noreply.github.com>
2022-06-23 20:17:21 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,38 @@
+
+
+
+
+$('.item.arriba , .electronica').mouseover(function(){
+
+    $('.electronica').addClass('activo')
+    
+})
+
+$('.item.arriba , .electronica').mouseout(function(){
+
+    $('.electronica').removeClass('activo')
+    
+})
+
+$('.item.asis , .asistencia').mouseover(function(){
+
+    $('.asistencia').addClass('activo')
+})
+
+$('.item.asis , .asistencia').mouseout(function(){
+
+    $('.asistencia').removeClass('activo')
+    
+})
+
+$('.item.sony , .inicio').mouseover(function(){
+
+    $('.inicio').addClass('activo')
+    
+})
+
+$('.item.sony , inicio').mouseout(function(){
+
+    $('.inicio').removeClass('activo')
+    
+})
